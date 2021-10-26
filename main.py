31500a1dd4bde348dd14417cdadcdff7a516694f
wai81 <50743966+wai81@users.noreply.github.com>
2021-10-26 21:32:05 +0300

    create and work urls

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from .schemas import Book
 
 app = FastAPI()
+
 
 @app.get('/')
 def home():
     return {"key": "Hello"}
+
+
+@app.get('/{pk}')
+def get_item(pk: int, q: str = None):
+    return {"key": pk, "q": q}
+
+
+@app.get('/user/{pk}/items/{item}/')
+def get_user_item(pk: int, item: str):
+    return {"user": pk, "item": item}
+
+
+
